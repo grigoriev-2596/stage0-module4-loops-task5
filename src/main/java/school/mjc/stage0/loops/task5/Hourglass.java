72e929c294c49f19911faecbed03e864ab658b15
grigoriev-2596 <grigoriev.egor2596@gmail.com>
2022-10-31 17:15:08 +0300

    all tasks are done

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,7 +1,25 @@
 package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
-    public void printHourglassOfGivenSize(int height) {
+    public static void printHourglassOfGivenSize(int height) {
+        for (int i = height; i > 0; i -= 2) {
+            printEights(i, height);
+        }
+        for (int i = height % 2 == 0 ? 2 : 3; i <= height; i += 2) {
+            printEights(i, height);
+        }
+    }
 
+    public static void printEights(int n, int length) {
+        final int SPACE_AMOUNT = length - n;
+        for (int i = 0; i < length; i++) {
+            if (i < SPACE_AMOUNT / 2 || i > length - SPACE_AMOUNT / 2 - 1) System.out.print(" ");
+            else System.out.print("8");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        printHourglassOfGivenSize(6);
     }
 }
